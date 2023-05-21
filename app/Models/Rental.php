d6912c530c3608b0fc6d745a53f3840670b566a2
Victor Romero <vsr.romero@gmail.com>
2023-05-21 17:17:13 +0100

    Made several changes into Rentals, from DB till code

--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Rental extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'customer_id',
+        'car_id',
+        'starting_date',
+        'planned_return_date',
+        'end_date',
+        'value',
+        'initial_ml',
+        'final_ml',
+    ];
+
+    public function rules($id = null) : array
+    {
+        return [
+            'customer_id' => 'required|exists:customers,id',
+            'car_id' => 'required|exists:cars,id',
+            'starting_date' => 'required|date',
+            'planned_return_date' => 'required|date',
+            'end_date' => 'date',
+            'value' => 'required|numeric',
+            'initial_ml' => 'required|integer',
+            'final_ml' => 'integer',
+        ];
+    }
 }
---
+++ app/Repositories/RentalRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Repositories;
+
+class RentalRepository extends Repository{
+
+}
+
+?>
